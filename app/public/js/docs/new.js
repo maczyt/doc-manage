f436012e2cf8b70715da1fb1c7a56cc448354ab5
maczyt <369280416@qq.com>
2018-11-10 14:00:25 +0800

    添加tui.editor及其依赖 简单markdown渲染模板demo

--- app/public/js/docs/new.js
+++ app/public/js/docs/new.js
@@ -1,5 +1,21 @@
+/* eslint-disable no-undef */
+
 'use strict';
 
 $(function () {
-  console.log('ok');
+
+  // init tui.edit
+  const editor = new tui.Editor({
+    el: document.querySelector('#editMain'),
+    initialEditType: 'markdown',
+    previewStyle: 'vertical',
+    height: '300px',
+  });
+
+  // 隐藏WYSIWYG，只显示MARKDOWN
+  editor.getUI().getModeSwitch().hide();
+
+  $('.submit').click(function () {
+    console.log($('.form').serializeArray());
+  });
 });
